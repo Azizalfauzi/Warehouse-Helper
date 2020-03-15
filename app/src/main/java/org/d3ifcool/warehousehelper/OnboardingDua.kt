1c228417da4984c43b08aca4e45f2aa11a704457
Azizalfauzi <azizalfauzi65@gmail.com>
2020-03-15 13:06:02 +0700

    menambahkan fungsi navigation

--- app/src/main/java/org/d3ifcool/warehousehelper/OnboardingDua.kt
+++ app/src/main/java/org/d3ifcool/warehousehelper/OnboardingDua.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import org.d3ifcool.warehousehelper.databinding.FragmentOnboardingDuaBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +19,9 @@ class OnboardingDua : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding_dua, container, false)
+        val binding = DataBindingUtil.inflate<FragmentOnboardingDuaBinding>(inflater,R.layout.fragment_onboarding_dua,container,false)
+            binding.btnNextdua.setOnClickListener { view:View->view.findNavController().navigate(R.id.action_onboardingDua_to_onboardingTiga) }
+        return binding.root
     }
 
 }
--- app/src/main/java/org/d3ifcool/warehousehelper/OnboardingSatu.kt
+++ app/src/main/java/org/d3ifcool/warehousehelper/OnboardingSatu.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import org.d3ifcool.warehousehelper.databinding.FragmentOnboardingSatuBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +19,16 @@ class OnboardingSatu : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding_satu, container, false)
+        val binding = DataBindingUtil.inflate<FragmentOnboardingSatuBinding>(
+            inflater,
+            R.layout.fragment_onboarding_satu,
+            container,
+            false
+        )
+        binding.btnNextsatu.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_onboardingSatu_to_onboardingDua)
+        }
+        return binding.root
     }
 
 }
